ec97ded72f750b90d8cf72b1ff9711f9dc27b215
AJ197028 <aksjais20@gmail.com>
2023-04-08 18:44:35 +0530

    by react router version 5

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import Heading from './Components/Layout/Heading';
 import MusicContent from './Components/Layout/MusicContent';
 import Navbar from './Components/Layout/Navbar';
 import CartProvider from './Components/Store/CartProvider';
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
 import About from './Components/Pages/About';
 import Home from './Components/Pages/Home';
 import HeaderContent from './Components/Layout/HeaderContent';
 import Contact from './Components/Pages/Contact';
+import { BrowserRouter, Route } from "react-router-dom";
 
 
 function App() {
@@ -24,48 +21,30 @@ function App() {
       setShowCart(true);
     }
   }
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (<>
-        <Navbar handleToggleCart={handleToggleCart} />
-        <HeaderContent />
-        <Home />
-        <Footer />
-      </>),
-    },
-    {
-      path: "/store",
-      element: (<>
-        <Navbar handleToggleCart={handleToggleCart} />
-        <Heading />
-        <MusicContent handleToggleCart={handleToggleCart} />
-        <Footer />
-      </>),
-    },
-    {
-      path: "/about",
-      element: <>
-        <Navbar handleToggleCart={handleToggleCart} />
-        <Heading />
-        <About />
-        <Footer />
-      </>,
-    },
-    {
-      path: "/contact",
-      element: <>
-        <Navbar handleToggleCart={handleToggleCart} />
-        <Contact />
-        <Footer />
-      </>,
-    },
-  ]);
+
   return (
-    <CartProvider>
-      {showCart && <Cart handleToggleCart={handleToggleCart}/>}
-      <RouterProvider router={router} />
-    </CartProvider>
+    <BrowserRouter>
+      <CartProvider>
+        {showCart && <Cart handleToggleCart={handleToggleCart} />}
+        <Navbar handleToggleCart={handleToggleCart} />
+        <Route exact path='/'>
+          <HeaderContent />
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <Heading />
+          <About />
+        </Route>
+        <Route exact path='/store'>
+          <Heading />
+          <MusicContent handleToggleCart={handleToggleCart} />
+        </Route>
+        <Route exact path='/contact'>
+          <Contact />
+        </Route>
+        <Footer />
+      </CartProvider>
+    </BrowserRouter>
   );
 }
 
